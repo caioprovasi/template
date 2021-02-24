1adfc209f3808fd21faf7856eae5ffbdb4f0c5dd
Caio Henrique Barros Provasi <39776736+caioprovasi@users.noreply.github.com>
2021-02-24 14:51:03 -0300

    teste

---
+++ issue28.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * The template for displaying issue 28
+ */
+
+get_header();
+?>
+
+	<section id="primary" class="content-area">
+		<main id="main" class="site-main">
+            <div class="news-stories">
+			</div>
+			<div class="showing-9-of-454-res">
+			</div>
+			<div class="sort-by">
+			</div>
+			<div class="latests">
+			</div>
+			<div class="rectangle-copy-21">
+			</div>
+			<div class="filter-the-list">
+			</div>
+			<div class="rectangle-copy-6">
+			</div>
+			<div class="rectangle-copy-7">
+			</div>
+			<div class="rectangle-copy-8">
+			</div>
+			<div class="rectangle-copy-18">
+			</div>
+			
+			
+			
+			
+			
+			
+			<div class="error-404">
+                <h1>
+                    <?php _e( 'Error', 'jeo' ); ?>
+                    <strong>404</strong>
+                </h1> 
+                <p>
+                    <?php _e( 'Oops! That page can&rsquo;t be found.', 'jeo' ); ?>
+                </p> 
+                <a href="/" class="button">
+                    <span><?php _e( 'Back to the home', 'jeo' ); ?></span>
+                </a>
+            </div>
+
+		</main><!-- #main -->
+	</section><!-- #primary -->
+
+<?php
+get_footer();
